a105db488637c57b13a2f616ba60b927a3bf3753
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2026-02-11 15:13:21 +0300

    Create onboarding complete route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware.js";
-import { register, login, getMe } from "../controllers/auth.controller.js";
+import { register, login, getMe, completeOnboarding } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe)
+router.post("/onboarding/complete", protect, completeOnboarding);
 
 export default router;
