f27d3f23240daf9131dc86d2571be7fe2c29fd2a
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2026-03-14 03:13:31 +0300

    removing dashboard and onboarding routes from auth.routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,10 @@
 import express from "express";
-import { protect } from "../middleware/auth.middleware.js";
-import { register, login, getMe, completeOnboarding } from "../controllers/auth.controller.js";
+import { register, login } from "../controllers/auth.controller.js";
 
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", protect, getMe)
-router.post("/onboarding/complete", protect, completeOnboarding);
 
 export default router;
