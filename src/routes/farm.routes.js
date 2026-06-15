8914e0e0928138579bb252a3a83f02cb6bcd5367
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2026-06-15 03:05:16 +0300

    Update farm.routes.js
    Adding the addCrop route

--- src/routes/farm.routes.js
+++ src/routes/farm.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware.js";
-import { updateFarm } from "../controllers/farm.controller.js";
+import { updateFarm, addCrop } from "../controllers/farm.controller.js";
 
 const router = express.Router();
 
 router.patch("/:farmId", protect, updateFarm);
+router.post("/:farmId/crops", protect, addCrop);
 
 export default router;
