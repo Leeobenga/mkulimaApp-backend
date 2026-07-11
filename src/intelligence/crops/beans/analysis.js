f6af5d68aa54e9c74c8de2190d9e1c7a3f891660
Leeobenga <111017790+Leeobenga@users.noreply.github.com>
2026-07-11 22:48:44 +0300

    Create analysis.js

---
+++ src/intelligence/crops/beans/analysis.js
@@ -0,0 +1,5 @@
+import { createAnalyzer, buildDriverCandidates } from "../../shared/cropAnalysis.js";
+import { getStageProfile, getStageWeight } from "./calibration.js";
+
+export { buildDriverCandidates };
+export const analyzeBeans = createAnalyzer({ getStageProfile, getStageWeight, cropType: "beans" });
